f30908af6d1ac93eab9f74a11aa4238dcc8c1cf3
Mohammed Basuliman <m7d.bas@gmail.com>
2021-12-03 18:22:07 +0300

    Add comments for explanation

--- src/com/company/Stadium.java
+++ src/com/company/Stadium.java
@@ -1,15 +1,18 @@
 package com.company;
 
 public class Stadium {
+    //Declare Variables
     private final String stadiumName;
     private Seat[] seats;
 
+    //Class constructor
     public Stadium(String stadiumName, int numRows, int seatsPerRow) {
         this.stadiumName = stadiumName;
 
         int numberOfSeats = numRows * seatsPerRow;
-        this.seats = new Seat[numberOfSeats];
+        this.seats = new Seat[numberOfSeats]; // Creation of an array that holds all the seats.
 
+        // To give each seat a number [e.g. A05].
         int lastRow = 'A' + (numRows - 1),
             i = 0;
         for (char row = 'A'; row <= lastRow; row++){
@@ -20,10 +23,12 @@ public class Stadium {
         }
     }
 
+    // Return the stadium name.
     public String getStadiumName() {
         return stadiumName;
     }
 
+    // Reserve a seat by checking the availability of that seat.
     public boolean reserve (String seatNumber){
         Seat seatToReserve = findSeat(seatNumber);
         if (seatToReserve != null){
@@ -32,6 +37,7 @@ public class Stadium {
         return false;
     }
 
+    // Cancel a reserved seat
     public boolean cancel (String seatNumber) {
         Seat seatToCancel = findSeat(seatNumber);
         if (seatToCancel != null) {
@@ -40,23 +46,24 @@ public class Stadium {
         return false;
     }
 
-    public Seat findSeat(String seatNumber){
+    // Search part.
+    private Seat findSeat(String seatNumber){
         int low = 0,
                 high = this.seats.length - 1;
 
-        while (low <= high){
-            int mid = (low + high) / 2;
-            Seat midValue = seats[mid];
-            int comparisonNumber = midValue.getSeatNumber().compareTo(seatNumber);
+        while (low <= high){ // Check if reach the end of the array.
+            int mid = (low + high) / 2; // get the mid of the array.
+            Seat midValue = seats[mid]; // create temp object to save the middle object.
+            int comparisonNumber = midValue.compareTo(seatNumber); // Compare the saved temp object with middle.
 
             if (comparisonNumber < 0){
-                low = mid + 1;
+                low = mid + 1; // move the low pointer to the middle.
             }
             else if (comparisonNumber > 0){
-                high = mid - 1;
+                high = mid - 1; // move the high pointer to the middle.
             }
             else{
-                return midValue;
+                return midValue; // the seat found.
             }
         }
         System.out.println("Seat " + seatNumber + " is not there!");
